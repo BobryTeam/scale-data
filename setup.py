a0cf4384123df2e1a63cb434115aa715ae24a696
vitosotdihaet <vitostikitos@gmail.com>
2024-05-28 22:57:35 +0300

    Set up a scale_data library

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scale_data',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='BobryTeam',
+    author_email='sinntexxx@gmail.com',
+    description='ScaleData data structure',
+    url='https://github.com/BobryTeam/scale-data',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
