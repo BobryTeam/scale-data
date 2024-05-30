000f858ef59e5b4f855168dc0357b2bc8103c9dc
Vitaly Klimenko <67521698+vitosotdihaet@users.noreply.github.com>
2024-05-30 13:55:27 +0300

    Update scale_data.py

--- scale_data/scale_data.py
+++ scale_data/scale_data.py
@@ -1,19 +1,19 @@
 class ScaleData:
-    def __init__(self, replica_count: int):
+    def __init__(self, replica_coefficient: float):
         '''
         Инициализация класса
         '''
-        self.replica_count = replica_count
+        self.replica_coefficient: float = replica_coefficient
 
     def __str__(self) -> str:
         '''
         Превращает скейл дату в строку
         '''
-        return str(self.replica_count)
+        return str(self.replica_coefficient)
 
 class ScaleDataFromStr(ScaleData):
     def __init__(self, string_data: str):
         '''
         Инициализация класса из строки
         '''
-        return super().__init__(int(string_data))
+        return super().__init__(float(string_data))
